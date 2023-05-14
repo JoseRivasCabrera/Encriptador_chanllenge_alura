8ad96716a5e9239757356e0606fa7129b9e66108
Rivas <jose.trabajo877@gmail.com>
2023-05-13 19:42:21 -0600

    se agregan cambios menores en el documento html y javascript

--- javascript.js
+++ javascript.js
@@ -2,3 +2,23 @@ function alerta () {
         alert("probando boton");
     }
 
+var e = enter
+var i = imes
+var a = ai
+var o = ober
+var u = ufat
+
+function encriptar () {
+
+}
+
+function desencriptar () {
+    
+}
+
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
